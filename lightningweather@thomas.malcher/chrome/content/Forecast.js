tmalch/lightningWeather

commit 6bc4c8b62149d93d7c9c730b3ea573d37d76e000

cleanup and smaller fixes

lightningweather@thomas.malcher/chrome/content/Forecast.js
@@ -79,6 +79,9 @@ function mergeForecastElements(e1, e2) {
     return merged
 }
 
+/*
+    A Forecast is a Sequence of ForecastElements
+ */
 var IForecast = {
     forEach: function (func) {
         this._data.forEach(func);
@@ -103,8 +106,6 @@ var IForecast = {
         if (!(other instanceof Forecast)) {
             other = new Forecast(other);
         }
-        //other.updateGranularity();
-        //this.updateGranularity();
 
         if (other.granularity != undefined && this.granularity != undefined && other.granularity != this.granularity) {
             logger.info("cannot combine, granularity is different " + other.granularity + " != " + this.granularity);
@@ -113,8 +114,6 @@ var IForecast = {
         other.forEach(function (elem) {
             this.add(elem);
         }.bind(this));
-
-        //this.updateGranularity();
     },
     add: function (elem) {
         let i = this._data.findIndex(function (e) {

lightningweather@thomas.malcher/chrome/content/index.js
@@ -44,9 +44,7 @@ Components.utils.import("resource://gre/modules/Log.jsm");
 let root_logger = Log.repository.getLogger("lightningweather");
 root_logger.addAppender(new Log.DumpAppender(new Log.BasicFormatter()));
 root_logger.level = Log.Level.Warn;
-
 let logger = Log.repository.getLogger("lightningweather.index");
-logger.level = Log.Level.Debug;
 
 var lightningweather = {
     views: null,
@@ -71,7 +69,7 @@ var lightningweather = {
                 }
             }else if (data == "icon_set"){
                 let icon_set = lightningweather.prefs.getCharPref("icon_set");
-                for (var key in lightningweather.views) {
+                for (let key in lightningweather.views) {
                     if (lightningweather.views.hasOwnProperty(key)) {
                         lightningweather.views[key].setIconBaseUrl("chrome://lightningweather/skin/"+icon_set+"/");
                     }
@@ -80,7 +78,7 @@ var lightningweather = {
             }else if (data == "units"){
                 let unit = lightningweather.prefs.getCharPref("units");
                 logger.debug("Selected Unit" + unit);
-                for (var key in lightningweather.views) {
+                for (let key in lightningweather.views) {
                     if (lightningweather.views.hasOwnProperty(key)) {
                         lightningweather.views[key].setTemperatureUnit(unit);
                     }
@@ -102,22 +100,21 @@ var lightningweather = {
     },
     onLoad: function () {
         lightningweather.prefs.addObserver("", lightningweather.prefObserver, false);
+
+        let icon_set = "default";
+        let temperature_unit = "C";
+        try {
+            temperature_unit = lightningweather.prefs.getCharPref("units");
+        }catch(e) { }
+        try {
+            icon_set = lightningweather.prefs.getCharPref("icon_set");
+        }catch(e) { }
         lightningweather.views = {
             "day": new HourlyViewWeatherModule(document.getElementById("day-view")),
             "week": new HourlyViewWeatherModule(document.getElementById("week-view")),
             "month": new MonthViewWeatherModule(document.getElementById("month-view")),
             "multiweek": new MonthViewWeatherModule(document.getElementById("multiweek-view"))
         };
-        try {
-            var icon_set = lightningweather.prefs.getCharPref("icon_set");
-        }catch(e) {
-            icon_set = "default"
-        }
-        try {
-            var temperature_unit = lightningweather.prefs.getCharPref("units");
-        }catch(e) {
-            temperature_unit = "C"
-        }
         for (let key in lightningweather.views) {
             if (lightningweather.views.hasOwnProperty(key)) {
                 let weather_mod = lightningweather.views[key];
@@ -186,6 +183,7 @@ var lightningweather = {
         if (lightningweather.forecast instanceof Forecast) {
             forecast.combine(lightningweather.forecast);
             lightningweather.forecast = forecast;
+            lightningweather.updateCurrentView();
             lightningweather.saveForecast();
         } else { // check storage
             lightningweather.storage.get(lightningweather.forecastModule.storeageId, function (forecast_data) {
@@ -197,8 +195,9 @@ var lightningweather = {
                     logger.debug("No forecast in Storage!");
                     let existing_forecast = new Forecast();
                 }
-                forecast.combine(lightningweather.forecast);
+                forecast.combine(existing_forecast);
                 lightningweather.forecast = forecast;
+                lightningweather.updateCurrentView();
                 lightningweather.saveForecast();
             });
         }

lightningweather@thomas.malcher/chrome/content/preferences.js
@@ -160,7 +160,7 @@ lightningweather_prefs = {
         }else{
             let error_msg_elem = document.getElementById("error_msg_container");
             if(this.location_list.itemCount == 0 || this.location_list.getItemAtIndex(0).value == NO_RESULTS_VALUE){
-                error_msg_elem.innerHTML = "Please enter a query";
+                error_msg_elem.innerHTML = "Please enter a valid query";
                 error_msg_elem.openPopup(this.location_list);
             }else{
                 error_msg_elem.innerHTML = "Please select a location";

commit a1fa91caf503f95c549a6cff48312716fca8985a

fix bug introduced in 6bc4c8b62149d
bug led to infinite api requests if the api fails

lightningweather@thomas.malcher/chrome/content/Forecast.js
@@ -243,30 +243,31 @@ function Forecast(data) {
 }
 
 
-logger = Log.repository.getLogger("lightningweather.provider");
+let provider_logger = Log.repository.getLogger("lightningweather.provider");
+
 BaseProvider.prototype.error = function () {
-    logger.info("request error " + this.req.status + " -- " + this.req.statusText);
+    provider_logger.info("request error " + this.req.status + " -- " + this.req.statusText);
     this.save_callback(new Forecast())
 };
 BaseProvider.prototype.success = function (event) {
-    logger.debug("got response ");
+    provider_logger.debug("got response ");
     let forecast = this.parseForecast(event.currentTarget);
-    logger.info("got forecast of length " + forecast.length);
+    provider_logger.info("got forecast of length " + forecast.length);
     this.save_callback(forecast);
 };
 BaseProvider.prototype.requestForecast = function () {
     if (this.url == null) {
-        logger.error("City not given");
+        provider_logger.error("City not given");
         this.save_callback(new Forecast())
     }
     if (this.req.readyState != 0 && this.req.readyState != 4) {
-        logger.trace("request already running - state: " + this.req.readyState);
+        provider_logger.trace("request already running - state: " + this.req.readyState);
         return;  // already waiting for a response -> no need to request it again
     }
-    logger.debug("going to request: " + this.url);
+    provider_logger.debug("going to request: " + this.url);
     this.req.open("GET", this.url);
     this.req.send();
-    logger.info("request sent");
+    provider_logger.info("request sent");
 };
 BaseProvider.prototype.parseForecast = function () {
     throw "NOT IMPLEMENTED"
@@ -289,7 +290,7 @@ function GeoLookup() {
     this.req.timeout = 2000;
 }
 GeoLookup.prototype.error = function (callback, event) {
-    logger.debug("request error " + this.req.status + " -- " + this.req.statusText);
+    provider_logger.debug("request error " + this.req.status + " -- " + this.req.statusText);
     callback([]);
     // clone request to remove all eventListeners
     let new_req = new XMLHttpRequest();
@@ -310,7 +311,7 @@ GeoLookup.prototype.locations = function (query, callback) {
     let q = "?q=select woeid, name, country, admin1,admin2,admin3, centroid, timezone from geo.places where text = \"" + query + "\" and placeTypeName = \"Town\"&format=json";
 
     if (this.req.readyState != 0 && this.req.readyState != 4) {
-        logger.debug("request already running - state: " + this.req.readyState);
+        provider_logger.debug("request already running - state: " + this.req.readyState);
         this.req.abort();
     }
     this.req.addEventListener("error", this.error.bind(this, callback));
@@ -324,14 +325,14 @@ GeoLookup.prototype.parseLocations = function (http_response) {
     try {
         var response = JSON.parse(http_response.responseText);
         if (response.error != undefined) {
-            logger.info("ERROR: " + http_response.responseText);
+            provider_logger.info("ERROR: " + http_response.responseText);
             return [];
         }
         if (response.query.results == null) {
             return [];
         }
     } catch (e) {
-        logger.error(e);
+        provider_logger.error(e);
         return [];
     }
 

lightningweather@thomas.malcher/chrome/content/index.js
@@ -46,6 +46,7 @@ root_logger.addAppender(new Log.DumpAppender(new Log.BasicFormatter()));
 root_logger.level = Log.Level.Warn;
 let logger = Log.repository.getLogger("lightningweather.index");
 
+
 var lightningweather = {
     views: null,
     storage: SimpleStorage.createCpsStyle("teste"),
@@ -177,8 +178,9 @@ var lightningweather = {
      * @param forecast
      */
     mergeForecast: function (forecast) {
-        if (!forecast) {
-            return;  // failed
+        if (!forecast || !(forecast instanceof Forecast) || forecast.length == 0) {
+            logger.debug("empty Forecast -> nothing to merge, nothing to update");
+            return;
         }
         if (lightningweather.forecast instanceof Forecast) {
             forecast.combine(lightningweather.forecast);
@@ -187,13 +189,12 @@ var lightningweather = {
             lightningweather.saveForecast();
         } else { // check storage
             lightningweather.storage.get(lightningweather.forecastModule.storeageId, function (forecast_data) {
+                let existing_forecast = new Forecast();
                 if (forecast_data) {
                     logger.debug("found forecast in Storage " + forecast_data.length);
-                    let existing_forecast = new Forecast(forecast_data);
-
+                    existing_forecast = new Forecast(forecast_data);
                 } else { // no forecast in object or storage -> request
                     logger.debug("No forecast in Storage!");
-                    let existing_forecast = new Forecast();
                 }
                 forecast.combine(existing_forecast);
                 lightningweather.forecast = forecast;